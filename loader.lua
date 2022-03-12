8d28b39de747dfa3e7d3e9e158caada2e18255bd
VastyyBtw <97897529+VastyyBtw@users.noreply.github.com>
2022-03-12 21:05:13 +1030

    Add files via upload

---
+++ loader.lua
@@ -0,0 +1,12 @@
+if game.PlaceId == 6725802681
+loadstring(game:HttpGet("https://raw.githubusercontent.com/VastyyBtw/CatWare/main/titan2.lua", true))()
+end
+
+if game.PlaceId == 8975757665
+loadstring(game:HttpGet("https://raw.githubusercontent.com/VastyyBtw/CatWare/main/titan2.lua", true))()
+end
+
+if game.PlaceId == 155615604
+loadstring(game:HttpGet("https://raw.githubusercontent.com/VastyyBtw/CatWare/main/prisonlife.lua", true))()
+end
+
